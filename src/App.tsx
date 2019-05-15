9b38c5c0363e184bc8eb7715f53b3fc1411d1e79
Nathan Chin <43153371+YozoraNoHoshi@users.noreply.github.com>
2019-05-15 14:03:06 -0700

    added timer

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Counter from './Counter';
@@ -6,12 +6,20 @@ import CounterHooks from './CounterHooks';
 import NumberFact from './NumberFact';
 import NumberFactHooks from './NumberFactHooks';
 import CounterForm from './CounterForm';
+import Timer from './Timer';
 
 const App: React.FC = () => {
-  const num = Math.floor(Math.random() * 100);
+  const [num] = useState(Math.floor(Math.random() * 100));
+  const [activeTimer, setActiveTimer] = useState(false);
   return (
     <div className="App">
+      {activeTimer && <Timer />}
+      <button onClick={() => setActiveTimer(!activeTimer)}>
+        Activate Timer
+      </button>
       <CounterForm />
+      <Counter />
+      <CounterHooks />
       <NumberFact number={num} />
       <NumberFactHooks number={num} />
     </div>
