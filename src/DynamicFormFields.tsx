fe87fab4a3dfaa1d759a515a9ac44521585b73d4
Nathan Chin <43153371+YozoraNoHoshi@users.noreply.github.com>
2019-05-23 12:16:37 -0700

    removed unused comments

--- src/DynamicFormFields.tsx
+++ src/DynamicFormFields.tsx
@@ -28,29 +28,14 @@ function DynamicFormHook(props: Props): JSX.Element {
   // but without a selective merge like Class component setState there isn't really a better way
   // Memoizes the onChange callback so hypothetical input components do not re-render
   const onChange = useCallback((evt: any): void => {
-    /*
-    Modifies existing state (bad based on setState from Class Components, but constant time complexity)
-    Will not trigger a re-render as React compares state by Object.is/Reference
-    Always replace state
-    setForm((frm: { [name: string]: string }): { [name: string]: string } => {
-      frm[evt.target.name] = evt.target.value;
-      return frm;
-    });
-    */
+    // Replaces state with new object. Linear time complexity, linear space complexity.
 
-    /*
-    Replaces state with new object. Linear time complexity, linear space complexity.
-    */
     setForm(
       (frm: { [name: string]: string }): { [name: string]: string } => {
         return { ...frm, [evt.target.name]: evt.target.value };
       }
     );
   }, []);
-
-  // const memoOnChange = useCallback((evt: any) => {
-  //   onChange(evt);
-  // }, []);
 
   // Only supports text inputs, no dropdowns/selects at the moment.
   const renderInputs = (inputs: Inputs[]): JSX.Element[] => {
